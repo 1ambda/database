c29100fc78f9ab9bddfb563ac339c7dc714d092c
1ambda <1amb4a@gmail.com>
2014-09-25 17:37:57 +0900

    Wokring: department

--- university.sql
+++ university.sql
@@ -1,3 +1,10 @@
+
+/*
+ * remove database `university`
+ */ 
+
+DROP DATABASE IF EXISTS `university`;
+CREATE DATABASE `university` DEFAULT CHARACTER SET=utf8;
 USE university;
 
 /*
@@ -5,6 +12,12 @@ USE university;
  */
 DROP TABLE IF EXISTS `student`;
 DROP TABLE IF EXISTS `instructor`;
+DROP TABLE IF EXISTS `department`;
+DROP TABLE IF EXISTS `advisor`;
+
+/*
+ * http://dba.stackexchange.com/questions/51855/advantages-of-middle-tables-in-one-to-many-relationships
+ */
 
 CREATE TABLE `instructor` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -40,3 +53,16 @@ INSERT INTO `student` (advisor_id, name, tot_cred) VALUES
 UPDATE `instructor` SET name="John Terry", id="5" WHERE id=4;
 DELETE FROM `instructor` WHERE id=1; 
 
+CREATE TABLE `department` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `dept_name` VARCHAR(50) NOT NULL,
+  `building` VARCHAR(50) NOT NULL,
+  `budget` VARCHAR(50) NOT NULL DEFAULT 0,
+  PRIMARY KEY(`id`),
+  UNIQUE KEY `ix_dept_name` (`dept_name`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+
+/*
+ *
+ *
+ /
